5b2829e886e922a5831648942e7f577a76ee7f36
Allan Evans <allan@aelyssum.com>
2016-12-07 16:22:33 -0800

    Initial Commit

---
+++ Textbox/Textbox.h
@@ -0,0 +1,19 @@
+//
+//  Textbox.h
+//  Textbox
+//
+//  Created by Allan Evans on 12/7/16.
+//  Copyright © 2016 Aelyssum Corp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Textbox.
+FOUNDATION_EXPORT double TextboxVersionNumber;
+
+//! Project version string for Textbox.
+FOUNDATION_EXPORT const unsigned char TextboxVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Textbox/PublicHeader.h>
+
+
---
+++ TextboxTests/TextboxTests.swift
@@ -0,0 +1,36 @@
+//
+//  TextboxTests.swift
+//  TextboxTests
+//
+//  Created by Allan Evans on 12/7/16.
+//  Copyright © 2016 Aelyssum Corp. All rights reserved.
+//
+
+import XCTest
+@testable import Textbox
+
+class TextboxTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
